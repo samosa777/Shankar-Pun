934d4093c34c94561afe0f2d999a95dfdf139caf
samosa777 <samosa2082@gmail.com>
2026-06-09 17:50:41 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBkRWPYZtmjS-lpiojtNtY_6h4IORZ6xjc",
+  authDomain: "hotel-menu-f25ed.firebaseapp.com",
+  projectId: "hotel-menu-f25ed",
+  storageBucket: "hotel-menu-f25ed.firebasestorage.app",
+  messagingSenderId: "750886705933",
+  appId: "1:750886705933:web:c3331f1dd8cfc99342ee44"
+};
+
+console.log("Firebase Config Loaded");
