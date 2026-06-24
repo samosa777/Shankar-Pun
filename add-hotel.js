c599f1860e45b2ee75c4cc7b5687fe0766d78b6d
samosa777 <samosa2082@gmail.com>
2026-06-24 17:46:16 +0545

    Update add-hotel.js

--- add-hotel.js
+++ add-hotel.js
@@ -1,16 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-getFirestore,
-collection,
-addDoc,
-serverTimestamp
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-import {
 getAuth,
-onAuthStateChanged,
-createUserWithEmailAndPassword
+signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
@@ -23,96 +15,33 @@ appId: "1:750886705933:web:c3331f1dd8cfc99342ee44"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 const auth = getAuth(app);
 
-onAuthStateChanged(auth, (user) => {
+document.getElementById("loginBtn").addEventListener("click", async () => {
 
-if (!user) {
-
-alert("Please Login First");
-window.location.href = "index.html";
-
-}
-
-});
-
-document
-.getElementById("createHotelBtn")
-.addEventListener("click", async () => {
-
-const hotelName = document.getElementById("hotelName").value;
-const ownerName = document.getElementById("ownerName").value;
-const mobile = document.getElementById("mobile").value;
-const whatsapp = document.getElementById("whatsapp").value;
-const email = document.getElementById("email").value;
+const email = document.getElementById("username").value;
 const password = document.getElementById("password").value;
-const address = document.getElementById("address").value;
-const city = document.getElementById("city").value;
-const state = document.getElementById("state").value;
-const country = document.getElementById("country").value;
-const maps = document.getElementById("maps").value;
-const website = document.getElementById("website").value;
-const facebook = document.getElementById("facebook").value;
-const instagram = document.getElementById("instagram").value;
-const plan = document.getElementById("plan").value;
-const startDate = document.getElementById("startDate").value;
-const status = document.getElementById("status").value;
-
-if (
-hotelName === "" ||
-ownerName === "" ||
-mobile === "" ||
-email === "" ||
-password === ""
-) {
-
-alert("Please Fill Required Fields");
-return;
-
-}
 
 try {
 
-const userCredential =
-await createUserWithEmailAndPassword(
+await signInWithEmailAndPassword(
 auth,
 email,
 password
 );
 
-const hotelUID = userCredential.user.uid;
+if(email === "superadmin@power.com"){
 
-await addDoc(
-collection(db, "hotels"),
-{
-hotelUID,
-hotelName,
-ownerName,
-mobile,
-whatsapp,
-email,
-address,
-city,
-state,
-country,
-maps,
-website,
-facebook,
-instagram,
-plan,
-startDate,
-status,
-createdAt: serverTimestamp()
-}
-);
+window.location.href = "dashboard.html";
 
-alert("Hotel Added Successfully");
+}else{
 
-window.location.reload();
+window.location.href = "hotel-dashboard.html";
 
 }
-catch (error) {
+
+}
+catch(error){
 
 alert(error.message);
 console.log(error);
